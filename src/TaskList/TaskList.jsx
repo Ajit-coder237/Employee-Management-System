393d1306221cd631c32a66fae441b659488ccf6e
Madhu Ajit Pandey <83753660+Ajit-coder237@users.noreply.github.com>
2024-11-06 13:22:52 +0545

    tasklist styled

--- src/TaskList/TaskList.jsx
+++ src/TaskList/TaskList.jsx
@@ -2,8 +2,25 @@ import React from "react";
 
 const TaskList = () => {
   return (
-    <div className="h-[55%] w-full py-5 bg-red-500 mt-10">
-      <div className="h-full w-[300px] bg-yellow-400 rounded-xl"></div>]
+    <div
+      id="tasklist"
+      className="flex overflow-x-auto justify-start items-center gap-5 flex-nowrap h-[55%] w-full py-5 mt-10"
+    >
+      <div className="flex-shrink-0 h-full w-[300px] p-5 bg-red-400 rounded-xl">
+        <div className="flex justify-between  items-center">
+          <h3 className="bg-red-500 text-sm px-3 py-1 rounded">High</h3>
+          <h4 className="text-base">6th November 2024</h4>
+        </div>
+        <h2 className="mt-5 text-xl font-semibold">
+          Make a youtube video about importance of web technology knowledge in
+          2024
+        </h2>
+      </div>
+      <div className="flex-shrink-0 h-full w-[300px] bg-yellow-400 rounded-xl"></div>
+      <div className="flex-shrink-0 h-full w-[300px] bg-yellow-400 rounded-xl"></div>
+      <div className="flex-shrink-0 h-full w-[300px] bg-yellow-400 rounded-xl"></div>
+      <div className="flex-shrink-0 h-full w-[300px] bg-yellow-400 rounded-xl"></div>
+      <div className="flex-shrink-0 h-full w-[300px] bg-yellow-400 rounded-xl"></div>
     </div>
   );
 };
