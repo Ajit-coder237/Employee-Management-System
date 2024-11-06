e4e4a6e8aebe531a5469c6b9e84de46dc1c1b1f0
Madhu Ajit Pandey <83753660+Ajit-coder237@users.noreply.github.com>
2024-11-06 11:12:07 +0545

    added headers and tasklistnumbers with styling

--- src/components/Auth/Dashboard/EmployeeDashboard.jsx
+++ src/components/Auth/Dashboard/EmployeeDashboard.jsx
@@ -1,9 +1,14 @@
-import React from 'react'
+import React from "react";
+import Header from "../../others/Header";
+import TaskListNumbers from "../../others/TaskListNumbers";
 
 const EmployeeDashboard = () => {
   return (
-    <div>EmployeeDashboard</div>
-  )
-}
+    <div className="p-10 bg-[#1C1C1C] h-screen">
+      <Header />
+      <TaskListNumbers />
+    </div>
+  );
+};
 
-export default EmployeeDashboard
+export default EmployeeDashboard;
