6b04690eb1f9e2167975c269d423795cd8d97dd2
Madhu Ajit Pandey <83753660+Ajit-coder237@users.noreply.github.com>
2024-11-06 15:23:27 +0545

    added create task in the ui

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,12 @@
 import React from "react";
 import Login from "./components/Auth/login";
 import EmployeeDashboard from "./components/Auth/Dashboard/EmployeeDashboard";
+import AdminDashboard from "./components/Auth/Dashboard/AdminDashboard";
 
 const App = () => {
   return (
     <>
-      {/* <Login /> */}
-      <EmployeeDashboard />
+      <AdminDashboard />
     </>
   );
 };
--- src/TaskList/TaskList.jsx
+++ src/TaskList/TaskList.jsx
@@ -4,10 +4,10 @@ const TaskList = () => {
   return (
     <div
       id="tasklist"
-      className="flex overflow-x-auto justify-start items-center gap-5 flex-nowrap h-[55%] w-full py-5 mt-10 "
+      className="flex overflow-x-auto justify-start items-center gap-5 flex-nowrap h-[55%] w-full py-5 mt-10"
     >
       <div className="flex-shrink-0 h-full w-[300px] p-5 bg-red-400 rounded-xl hover:bg-emerald-500 cursor-pointer">
-        <div className="flex justify-between  items-center">
+        <div className="flex justify-between items-center">
           <h3 className="bg-red-500 text-sm px-3 py-1 rounded">High</h3>
           <h4 className="text-sm">6th November 2024</h4>
         </div>
@@ -18,8 +18,9 @@ const TaskList = () => {
           quia culpa voluptates distinctio assumenda sit.
         </p>
       </div>
+
       <div className="flex-shrink-0 h-full w-[300px] p-5 bg-green-400 rounded-xl hover:bg-emerald-500 cursor-pointer">
-        <div className="flex justify-between  items-center">
+        <div className="flex justify-between items-center">
           <h3 className="bg-red-500 text-sm px-3 py-1 rounded">High</h3>
           <h4 className="text-sm">6th November 2024</h4>
         </div>
@@ -30,8 +31,9 @@ const TaskList = () => {
           quia culpa voluptates distinctio assumenda sit.
         </p>
       </div>
+
       <div className="flex-shrink-0 h-full w-[300px] p-5 bg-blue-400 rounded-xl hover:bg-emerald-500 cursor-pointer">
-        <div className="flex justify-between  items-center">
+        <div className="flex justify-between items-center">
           <h3 className="bg-red-500 text-sm px-3 py-1 rounded">High</h3>
           <h4 className="text-sm">6th November 2024</h4>
         </div>
@@ -42,8 +44,9 @@ const TaskList = () => {
           quia culpa voluptates distinctio assumenda sit.
         </p>
       </div>
+
       <div className="flex-shrink-0 h-full w-[300px] p-5 bg-yellow-600 rounded-xl hover:bg-emerald-500 cursor-pointer">
-        <div className="flex justify-between  items-center">
+        <div className="flex justify-between items-center">
           <h3 className="bg-red-500 text-sm px-3 py-1 rounded">High</h3>
           <h4 className="text-sm">6th November 2024</h4>
         </div>
@@ -54,8 +57,9 @@ const TaskList = () => {
           quia culpa voluptates distinctio assumenda sit.
         </p>
       </div>
+
       <div className="flex-shrink-0 h-full w-[300px] p-5 bg-gray-400 rounded-xl hover:bg-emerald-500 cursor-pointer">
-        <div className="flex justify-between  items-center">
+        <div className="flex justify-between items-center">
           <h3 className="bg-red-500 text-sm px-3 py-1 rounded">High</h3>
           <h4 className="text-sm">6th November 2024</h4>
         </div>
@@ -69,4 +73,5 @@ const TaskList = () => {
     </div>
   );
 };
+
 export default TaskList;
