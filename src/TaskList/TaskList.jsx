171a0e05bce6b366d8c269605474945a369bad35
Madhu Ajit Pandey <83753660+Ajit-coder237@users.noreply.github.com>
2024-11-06 13:36:26 +0545

    tasklist updated

--- src/TaskList/TaskList.jsx
+++ src/TaskList/TaskList.jsx
@@ -4,23 +4,68 @@ const TaskList = () => {
   return (
     <div
       id="tasklist"
-      className="flex overflow-x-auto justify-start items-center gap-5 flex-nowrap h-[55%] w-full py-5 mt-10"
+      className="flex overflow-x-auto justify-start items-center gap-5 flex-nowrap h-[55%] w-full py-5 mt-10 "
     >
-      <div className="flex-shrink-0 h-full w-[300px] p-5 bg-red-400 rounded-xl">
+      <div className="flex-shrink-0 h-full w-[300px] p-5 bg-red-400 rounded-xl hover:bg-emerald-500 cursor-pointer">
         <div className="flex justify-between  items-center">
           <h3 className="bg-red-500 text-sm px-3 py-1 rounded">High</h3>
-          <h4 className="text-base">6th November 2024</h4>
+          <h4 className="text-sm">6th November 2024</h4>
         </div>
-        <h2 className="mt-5 text-xl font-semibold">
-          Make a youtube video about importance of web technology knowledge in
-          2024
-        </h2>
+        <h2 className="mt-5 text-2xl font-semibold">Make a youtube video</h2>
+        <p className="text-sm mt-2">
+          Lorem ipsum dolor sit, amet consectetur adipisicing elit. Provident
+          quo veritatis dolor repellendus iure soluta tenetur reprehenderit,
+          quia culpa voluptates distinctio assumenda sit.
+        </p>
       </div>
-      <div className="flex-shrink-0 h-full w-[300px] bg-yellow-400 rounded-xl"></div>
-      <div className="flex-shrink-0 h-full w-[300px] bg-yellow-400 rounded-xl"></div>
-      <div className="flex-shrink-0 h-full w-[300px] bg-yellow-400 rounded-xl"></div>
-      <div className="flex-shrink-0 h-full w-[300px] bg-yellow-400 rounded-xl"></div>
-      <div className="flex-shrink-0 h-full w-[300px] bg-yellow-400 rounded-xl"></div>
+      <div className="flex-shrink-0 h-full w-[300px] p-5 bg-green-400 rounded-xl hover:bg-emerald-500 cursor-pointer">
+        <div className="flex justify-between  items-center">
+          <h3 className="bg-red-500 text-sm px-3 py-1 rounded">High</h3>
+          <h4 className="text-sm">6th November 2024</h4>
+        </div>
+        <h2 className="mt-5 text-2xl font-semibold">Make a youtube video</h2>
+        <p className="text-sm mt-2">
+          Lorem ipsum dolor sit, amet consectetur adipisicing elit. Provident
+          quo veritatis dolor repellendus iure soluta tenetur reprehenderit,
+          quia culpa voluptates distinctio assumenda sit.
+        </p>
+      </div>
+      <div className="flex-shrink-0 h-full w-[300px] p-5 bg-blue-400 rounded-xl hover:bg-emerald-500 cursor-pointer">
+        <div className="flex justify-between  items-center">
+          <h3 className="bg-red-500 text-sm px-3 py-1 rounded">High</h3>
+          <h4 className="text-sm">6th November 2024</h4>
+        </div>
+        <h2 className="mt-5 text-2xl font-semibold">Make a youtube video</h2>
+        <p className="text-sm mt-2">
+          Lorem ipsum dolor sit, amet consectetur adipisicing elit. Provident
+          quo veritatis dolor repellendus iure soluta tenetur reprehenderit,
+          quia culpa voluptates distinctio assumenda sit.
+        </p>
+      </div>
+      <div className="flex-shrink-0 h-full w-[300px] p-5 bg-yellow-600 rounded-xl hover:bg-emerald-500 cursor-pointer">
+        <div className="flex justify-between  items-center">
+          <h3 className="bg-red-500 text-sm px-3 py-1 rounded">High</h3>
+          <h4 className="text-sm">6th November 2024</h4>
+        </div>
+        <h2 className="mt-5 text-2xl font-semibold">Make a youtube video</h2>
+        <p className="text-sm mt-2">
+          Lorem ipsum dolor sit, amet consectetur adipisicing elit. Provident
+          quo veritatis dolor repellendus iure soluta tenetur reprehenderit,
+          quia culpa voluptates distinctio assumenda sit.
+        </p>
+      </div>
+      <div className="flex-shrink-0 h-full w-[300px] p-5 bg-gray-400 rounded-xl hover:bg-emerald-500 cursor-pointer">
+        <div className="flex justify-between  items-center">
+          <h3 className="bg-red-500 text-sm px-3 py-1 rounded">High</h3>
+          <h4 className="text-sm">6th November 2024</h4>
+        </div>
+        <h2 className="mt-5 text-2xl font-semibold">Make a youtube video</h2>
+        <p className="text-sm mt-2">
+          Lorem ipsum dolor sit, amet consectetur adipisicing elit. Provident
+          quo veritatis dolor repellendus iure soluta tenetur reprehenderit,
+          quia culpa voluptates distinctio assumenda sit.
+        </p>
+      </div>
     </div>
   );
 };
