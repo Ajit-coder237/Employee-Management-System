1180b774e2d589833e2856ca8164eba79b8b3919
Madhu Ajit Pandey <83753660+Ajit-coder237@users.noreply.github.com>
2024-11-08 21:53:42 +0545

    local storage updated

--- src/components/others/CreateTask.jsx
+++ src/components/others/CreateTask.jsx
@@ -1,14 +1,38 @@
-import React from "react";
+import React, { useState } from "react";
+// import { useRef } from "react";
 
 const CreateTask = () => {
+  // const inputRef = useRef(null);
+  // const focusInput = (event) => {
+  //   event.preventDefault();
+  //   inputRef.current.focus();
+  // };
+  const [isFocused, setIsFocused] = useState(false);
+  
+
+  const handleClick = () => {
+    setIsFocused(true);   
+  };
+  const handleBlur = () => {
+    setIsFocused(false);
+  };
+
   return (
     <div className="p-5 bg-[#1C1C1C] mt-7 rounded">
       <form className="flex flex-wrap w-full items-start justify-between">
         <div className="w-1/2">
           <div>
             <h3 className="text-sm text-gray-300 mb-0.5">Task Title</h3>
+            {/* <input ref={inputRef} type="text" placeholder="Focus on Me" />
+            <button onClick={focusInput}>Focus</button> */}
             <input
-              className="text-sm py-1 px-2 w-4/5 rounded outline-none bg-transparent border-[1px] border-gray-400 mb-4"
+              onClick={handleClick}
+              onBlur={handleBlur}
+              className={`text-sm py-1 px-2 w-4/5 rounded outline-none mb-4 ${
+                isFocused
+                  ? " bg-blue-100 border-[3px] border-blue-600 shadow-md"
+                  : "bg-transparent border-[1px] border-gray-400"
+              }`}
               type="text"
               placeholder="Make an UI design"
             />
